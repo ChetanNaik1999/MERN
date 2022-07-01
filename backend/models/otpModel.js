16bab5d1d7e2cb3b81d833e707174656c45845e6
chetan <chetannnaik775@gmail.com>
2022-07-01 12:05:39 +0530

    Backend Update

--- backend/models/otpModel.js
+++ backend/models/otpModel.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const otpSchema =new mongoose.Schema({
     email:{ type:String,required:true},
     otp:{type:Number,required:true},
-    expireIn:{type:Number}
+    expireIn:{type:Number,required:true}
 },{
     timestamps:true
 });
